e93e3c7f1eb69e96a612b6cea9a9cd11c15b71a9
IlyaKashapov <91538334+IlyaKashapov@users.noreply.github.com>
2023-04-04 20:24:10 +0300

    Add files via upload

---
+++ New_Plot/Plot/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Plot
+{
+    using System.Windows.Forms.DataVisualization;
+
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            pic = new PictureBox();
+            dek_x = new Label();
+            dek_y = new Label();
+            ekr_x = new Label();
+            ekr_y = new Label();
+            minx_dek = new TextBox();
+            maxx_dek = new TextBox();
+            xxx = new Label();
+            miny_dek = new TextBox();
+            maxy_dek = new TextBox();
+            label4 = new Label();
+            textfunc = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)pic).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button1.Location = new Point(713, 162);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 81);
+            button1.TabIndex = 1;
+            button1.Text = "Построить график";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // pic
+            // 
+            pic.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pic.BackColor = SystemColors.ButtonFace;
+            pic.Location = new Point(12, 74);
+            pic.Name = "pic";
+            pic.Size = new Size(695, 453);
+            pic.TabIndex = 2;
+            pic.TabStop = false;
+            pic.Click += pic_Click;
+            pic.MouseMove += pic_MouseMove;
+            // 
+            // dek_x
+            // 
+            dek_x.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dek_x.AutoSize = true;
+            dek_x.Location = new Point(12, 533);
+            dek_x.Name = "dek_x";
+            dek_x.Size = new Size(44, 20);
+            dek_x.TabIndex = 3;
+            dek_x.Text = "dek x";
+            // 
+            // dek_y
+            // 
+            dek_y.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dek_y.AutoSize = true;
+            dek_y.Location = new Point(145, 533);
+            dek_y.Name = "dek_y";
+            dek_y.Size = new Size(44, 20);
+            dek_y.TabIndex = 4;
+            dek_y.Text = "dek y";
+            dek_y.Click += label2_Click;
+            // 
+            // ekr_x
+            // 
+            ekr_x.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            ekr_x.AutoSize = true;
+            ekr_x.Location = new Point(493, 533);
+            ekr_x.Name = "ekr_x";
+            ekr_x.Size = new Size(40, 20);
+            ekr_x.TabIndex = 5;
+            ekr_x.Text = "x ekr";
+            // 
+            // ekr_y
+            // 
+            ekr_y.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            ekr_y.AutoSize = true;
+            ekr_y.Location = new Point(560, 533);
+            ekr_y.Name = "ekr_y";
+            ekr_y.Size = new Size(40, 20);
+            ekr_y.TabIndex = 6;
+            ekr_y.Text = "y ekr";
+            // 
+            // minx_dek
+            // 
+            minx_dek.Location = new Point(419, 41);
+            minx_dek.Name = "minx_dek";
+            minx_dek.Size = new Size(43, 27);
+            minx_dek.TabIndex = 9;
+            minx_dek.Text = "-2";
+            // 
+            // maxx_dek
+            // 
+            maxx_dek.Location = new Point(509, 41);
+            maxx_dek.Name = "maxx_dek";
+            maxx_dek.Size = new Size(44, 27);
+            maxx_dek.TabIndex = 10;
+            maxx_dek.Text = "2";
+            maxx_dek.TextChanged += maxx_dek_TextChanged;
+            // 
+            // xxx
+            // 
+            xxx.AutoSize = true;
+            xxx.Location = new Point(419, 9);
+            xxx.Name = "xxx";
+            xxx.Size = new Size(134, 20);
+            xxx.TabIndex = 11;
+            xxx.Text = "Диапазон x(в дек)";
+            // 
+            // miny_dek
+            // 
+            miny_dek.Location = new Point(595, 41);
+            miny_dek.Name = "miny_dek";
+            miny_dek.Size = new Size(35, 27);
+            miny_dek.TabIndex = 12;
+            miny_dek.Text = "-1";
+            // 
+            // maxy_dek
+            // 
+            maxy_dek.Location = new Point(650, 41);
+            maxy_dek.Name = "maxy_dek";
+            maxy_dek.Size = new Size(42, 27);
+            maxy_dek.TabIndex = 13;
+            maxy_dek.Text = "1";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(595, 9);
+            label4.Name = "label4";
+            label4.Size = new Size(134, 20);
+            label4.TabIndex = 16;
+            label4.Text = "Диапазон у(в дек)";
+            // 
+            // textfunc
+            // 
+            textfunc.Location = new Point(713, 128);
+            textfunc.Name = "textfunc";
+            textfunc.Size = new Size(75, 27);
+            textfunc.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 562);
+            Controls.Add(textfunc);
+            Controls.Add(label4);
+            Controls.Add(maxy_dek);
+            Controls.Add(miny_dek);
+            Controls.Add(xxx);
+            Controls.Add(maxx_dek);
+            Controls.Add(minx_dek);
+            Controls.Add(ekr_y);
+            Controls.Add(ekr_x);
+            Controls.Add(dek_y);
+            Controls.Add(dek_x);
+            Controls.Add(pic);
+            Controls.Add(button1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pic).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private PictureBox pic;
+        private Label dek_x;
+        private Label dek_y;
+        private Label ekr_x;
+        private Label ekr_y;
+        private TextBox minx_dek;
+        private TextBox maxx_dek;
+        private Label xxx;
+        private TextBox miny_dek;
+        private TextBox maxy_dek;
+        private Label label4;
+        private TextBox textfunc;
+    }
+}
+
---
+++ New_Plot/Plot/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System.ComponentModel;
+
+namespace Plot
+{
+    public partial class Form1 : Form
+    {
+        double min_x;
+        double max_x = 10;
+        double h = 0.1;
+        int wdth;
+        int hght;
+        Pen pen = new Pen(Color.Black);
+        Bitmap bm;
+        Graphics g;
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+        }
+        //int pix_in_one_x = 1;
+        //Bitmap map = new Bitmap(100, 100);
+        //Graphics g;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            min_x = Convert.ToDouble(minx_dek.Text.Replace(',', '.'));
+            max_x = Convert.ToDouble(maxx_dek.Text.Replace(',', '.'));
+            hght = pic.Height;
+            wdth = pic.Width;
+            double pixels = wdth / (max_x - min_x);
+            double len_y = hght / pixels;
+            bm = new Bitmap(pic.Width, pic.Height); ;
+            g = Graphics.FromImage(bm);
+
+            g.Clear(Color.White);
+            pic.Image = bm;
+            
+            g.DrawLine(pen, new Point(0, hght / 2), new Point(wdth, hght / 2));
+            g.DrawLine(pen, new Point(Convert.ToInt32(Math.Abs(min_x) * pixels), 0), new Point(Convert.ToInt32(Math.Abs(min_x) * pixels), hght));
+
+        }
+        Koordinate Koord = new Koordinate(0, 0, 0, 0, 1, 1);
+        private void pic_MouseMove(object sender, MouseEventArgs e)
+        {
+
+
+            ekr_y.Text = e.Y.ToString();
+            dek_y.Text = Koord.Convert_To_Y_dek(e.Y, miny_dek.Text, maxy_dek.Text, pic).ToString();
+            ekr_x.Text = e.X.ToString();
+            dek_x.Text = Koord.Convert_To_X_dek(e.X, minx_dek.Text, maxx_dek.Text, pic).ToString();
+
+
+            //map.SetPixel(e.X, e.Y, Color.Black);
+            //setpixel();
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int step = 1;
+            Function.GetFunction(textfunc.Text);
+            for (int i = 0; i < wdth - step; i += step)
+            {
+                
+
+                Koordinate k = new Koordinate();
+                var x_dek = k.Convert_To_X_dek(i, minx_dek.Text, maxx_dek.Text, pic);
+                var y = Convert.ToInt32(Function.FuncValue(x_dek));
+                var x_dek_next = k.Convert_To_X_dek(i+step, minx_dek.Text, maxx_dek.Text, pic);
+                var y_next = Convert.ToInt32(Function.FuncValue(x_dek_next));
+                g.DrawLine(pen, new Point(k.Convert_To_X_ekr(x_dek, minx_dek.Text, maxx_dek.Text, pic), 
+                           y), new Point(k.Convert_To_X_ekr(x_dek_next, minx_dek.Text, maxx_dek.Text, pic),
+                           y_next));
+
+            }
+
+        }
+
+        private void pic_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void minx_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maxx_dek_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        /*private void textBox1_TextChanged(object sender, EventArgs e)
+        { if (textBox1.Text != "")
+            {
+                double x_dek = Convert.ToDouble(textBox1.Text);
+                string minx = minx_dek.Text;
+                string maxx = maxx_dek.Text;
+                int x_ekr = Koord.Convert_To_X_ekr(x_dek, minx, maxx, pic);
+                dek_y.Text = x_ekr.ToString();
+            }
+        }*/
+
+
+
+    }
+}
+
+
